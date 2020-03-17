d090476dfb2ef0b9c2e59c0b520d20bafeaa3d80
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-03-17 14:37:40 +0500

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -32,59 +32,59 @@ def greet_user(response):
     if response:
         for row in greetings[0]:
             if response in row:
-                Voice.speak_mimic("Hello I am Dom", "Nice To meet You")
+                Voice.speak_flite("Hello I am Dom", "Nice To meet You")
                 return
         for row in greetings[1]:
             if response in row:
-                Voice.speak_mimic("I am Fine", "How About You?")
+                Voice.speak_flite("I am Fine", "How About You?")
                 return
         for row in greetings[2]:
             if response in row:
-                Voice.speak_mimic("Nothing much")
+                Voice.speak_flite("Nothing much")
                 return
         for row in greetings[3]:
             if response in row:
-                Voice.speak_mimic("Everything is fine")
+                Voice.speak_flite("Everything is fine")
                 return
         for row in greetings[4]:
             if response in row:
-                Voice.speak_mimic("My Day is Just Fine")
+                Voice.speak_flite("My Day is Just Fine")
                 return
                     
         for row in greetings[-2]:
             if response in row:
-                Voice.speak_mimic("Nice to Meet you too")
+                Voice.speak_flite("Nice to Meet you too")
                 return
         if "how have you been" in response:
-            Voice.speak_mimic("I have been fine", "how about you")
+            Voice.speak_flite("I have been fine", "how about you")
         if "morning" in response or "afternoon" in response or "evening" in response:
             hour = datetime.datetime.now().hour
             greeting = "Its morning", "Good morning" if 5 <= hour < 12 else "Its Afternoon", "Good afternoon" if hour < 18 else "Its Evening", "Good evening"
-            Voice.speak_mimic(greeting)
+            Voice.speak_flite(greeting)
             return
         if "hello" in response:
-            Voice.speak_mimic("Hello I am Dom", "Nice To meet You")
+            Voice.speak_flite("Hello I am Dom", "Nice To meet You")
             return
         if "how are you" in response:
-            Voice.speak_mimic("I have been fine", "what about you")
+            Voice.speak_flite("I have been fine", "what about you")
             return
         if "fine" in response:
             if "i am" in response:
-                Voice.speak_mimic("Good To Know")
+                Voice.speak_flite("Good To Know")
                 return
             if "are you" in response:
-                Voice.speak_mimic("Yes I am Fine")
+                Voice.speak_flite("Yes I am Fine")
                 return
         if "ok" in response:
             if "i am" in response:
-                Voice.speak_mimic("Good To Know")
+                Voice.speak_flite("Good To Know")
                 return
             if "are you" in response:
-                Voice.speak_mimic("Yes I am Fine")
+                Voice.speak_flite("Yes I am Fine")
                 return
         if "who are you" in response:
-            Voice.speak_mimic("I am Dom", "Dom stands for Data Operating Module")
+            Voice.speak_flite("I am Dom", "Dom stands for Data Operating Module")
             return
         if "who made you" in response:
-            Voice.speak_mimic("I am Just A Home Project")
+            Voice.speak_flite("I am Just A Home Project")
             return
