1c57c10227e38f166d72e935c948df635e654167
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-03-17 14:40:03 +0500

    Update search.py

--- search.py
+++ search.py
@@ -4,4 +4,4 @@ def search_wikipedia(search):
     if "who is" in search or "who was" in search or "who are" in search or "what is" in search:
         search = search.split()[2:]
         search = " ".join(search)
-        Voice.speak_mimic(wikipedia.summary(search, sentences=2))
+        Voice.speak_flite(wikipedia.summary(search, sentences=2))
