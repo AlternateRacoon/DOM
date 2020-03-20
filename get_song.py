59fadcffce40081ace016afa7f0579028197225c
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-03-20 19:40:29 +0500

    Update get_song.py

--- get_song.py
+++ get_song.py
@@ -16,7 +16,7 @@ def play_video(video_name):
     url = "https://www.youtube.com/watch?v=" + search_results[0]
     video = pafy.new(url)
     best = video.getbest()
-    cmd = ['mplayer','-novideo','-ao','oss','-slave', '-quiet', best.url]
+    cmd = ['mplayer','-novideo','-ao','oss', best.url]
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     sleep(5)
     while True:
@@ -39,26 +39,29 @@ def play_song(song_name):
         urllib.request.Request(link,
                                headers={'User-Agent': 'Mozilla/5.0'})).read().decode(
         "utf8")
-    mp3_link = re.findall('<a href="(.*?)" download="" class="btn btn-block btn-default">', html_data)[0]
-    
-    cmd = ['mplayer', '-ao', 'oss', '-slave', '-quiet', mp3_link]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    sleep(5)
-    while True:
-        command = Recognize.get_recognize_google()
-        print(command)
-        if command == False:
-            pass
-        else:
-            if "pause" in command or "stop" in command or "exit" in command or "quit" in command or "top" in command:
-                os.system("pkill mplayer")
-                break
-            elif "download to server" in command or "save to server" in command or "download" in command:
-                os.system("pkill mplayer")
-                os.system("sudo mount -t cifs -o user=root,pass=dietpi //192.168.1.111/dietpi /Dietpi")
-                Voice.speak_flite("Saving The File")
-                os.system("cd /Dietpi && wget '"+ mp3_link + "'")
-                break
+    if "<h4>Your search did not yield any results.</h4>" in html_data:
+        Voice.speak_flite("I am afraid, i cannot find any songs of that name")
+    else:
+        mp3_link = re.findall('<a href="(.*?)" download="" class="btn btn-block btn-default">', html_data)[0]
+
+        cmd = ['mplayer','-novideo', '-ao', 'oss', mp3_link]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        sleep(5)
+        while True:
+            command = Recognize.get_recognize_google()
+            print(command)
+            if command == False:
+                pass
+            else:
+                if "pause" in command or "stop" in command or "exit" in command or "quit" in command or "top" in command:
+                    os.system("pkill mplayer")
+                    break
+                elif "download to server" in command or "save to server" in command or "download" in command:
+                    os.system("pkill mplayer")
+                    os.system("sudo mount -t cifs -o user=root,pass=dietpi //192.168.1.111/dietpi /Dietpi")
+                    Voice.speak_flite("Saving The File")
+                    os.system("cd /Dietpi && wget '"+ mp3_link + "'")
+                    break
 
 
 
