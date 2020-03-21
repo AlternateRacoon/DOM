c183e612a1e404985ac7c429deda94911abdc7b8
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-03-21 15:20:59 +0500

    Update search.py

--- search.py
+++ search.py
@@ -2,16 +2,31 @@ import wikipedia
 from speak import Voice
 import random
 def search_wikipedia(search):
-    if "who is" in search or "who was" in search or "who are" in search or "what is" in search:
-        Voice.speak_flite("Searching About it")
+    if "who is" in search or "who was" in search or "who are" in search or "what is" in search or "what was" in search or "history of" in search:
         search = search.split()[2:]
         search = " ".join(search)
+        Voice.speak_flite("Searching about "+ search)
+        info = ""
         try:
             info = wikipedia.summary(search, sentences=2)
         except wikipedia.DisambiguationError as e:
             s = random.choice(e.options)
             info = wikipedia.summary(s, sentences=2)
         except wikipedia.PageError as e:
-            Voic.speak_flite("Could not find any results")
+            Voice.speak_flite("Could not find any results")
         if info:
             Voice.speak_flite(info)
+    elif "tell me about" in search:
+        search = " ".join(search.split()[2:])
+        Voice.speak_flite("Searching about "+ search)
+                info = ""
+        try:
+            info = wikipedia.summary(search, sentences=2)
+        except wikipedia.DisambiguationError as e:
+            s = random.choice(e.options)
+            info = wikipedia.summary(s, sentences=2)
+        except wikipedia.PageError as e:
+            Voice.speak_flite("Could not find any results")
+        if info:
+            Voice.speak_flite(info)   
+     
