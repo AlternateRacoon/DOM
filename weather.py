521bac47662a7e0ed505b0391ea470b6c404e303
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-03-22 15:51:49 +0500

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,23 @@
+import requests
+
+from speak import Voice
+
+def get_weather():
+    response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Karachi&APPID=07bbb96304be02263bcfa5534021d1c6")
+
+    x = response.json()
+
+    y = x["main"]
+
+    current_temperature = y["temp"]
+
+    current_humidity = y["humidity"]
+
+    z = x["weather"]
+
+    weather_description = z[0]["description"]
+    temprature = str(int(current_temperature - 273.15)) + "°C"
+    humidity = str(current_humidity) + "%"
+    Voice.speak_flite("Currently it is "+ temprature +" outside")
+    Voice.speak_flite("The description being "+ weather_description)
+    Voice.speak_flite("The Humidity is about "+ humidity)
