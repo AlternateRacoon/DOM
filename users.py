b6b25a29e3d7b3f26c0611d8dbb71221d2993800
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-03-24 19:07:44 +0500

    Update users.py

--- users.py
+++ users.py
@@ -28,18 +28,18 @@ Birthday = ""
 
 def user_stuff(response):
     if "create user" in response:
-        Voice.speak_mimic("Say The Users Name You Want to Create")
+        Voice.speak_flite("Say The Users Name You Want to Create")
         name = Recognize.get_recognize_google()
-        Voice.speak_mimic("Say The Users Age")
+        Voice.speak_flite("Say The Users Age")
         age = Recognize.get_recognize_google()
-        Voice.speak_mimic("Say The Users Birthday")
+        Voice.speak_flite("Say The Users Birthday")
         birthday = Recognize.get_recognize_google()
-        Voice.speak_mimic("Name, " + name + " Age, " + age + " Birthday, " + birthday)
-        Voice.speak_mimic("Are you sure you want to create this user")
+        Voice.speak_flite("Name, " + name + " Age, " + age + " Birthday, " + birthday)
+        Voice.speak_flite("Are you sure you want to create this user")
         response = Recognize.get_recognize_google()
         print(response)
         if "yes" in response:
-            Voice.speak_mimic("Creating User")
+            Voice.speak_flite("Creating User")
             create_user(name, age, birthday)
     elif "I am" in response:
         for row in get_all_users():
@@ -50,27 +50,29 @@ def user_stuff(response):
                     Age = row[1]
                     Birthday = row[2]
                     if row[0] == "Zara":
-                        Voice.speak_mimic("Hello There Zarah, Welcome Back")
+                        Voice.speak_flite("Hello There Zarah, Welcome Back")
                     elif row[0] == "Ayaan":
-                        Voice.speak_mimic("Hello There Aion, Welcome Back")
+                        Voice.speak_flite("Hello There Aion, Welcome Back")
+                    else:
+                        Voice.speak_flite("Hello There "+ row[0] +", Welcome Back")
     elif "what is my birthday" in response or "when is my birthday" in response:
         if Birthday:
-            Voice.speak_mimic("Your Birthday comes on " + Birthday)
+            Voice.speak_flite("Your Birthday comes on " + Birthday)
         else:
-            Voice.speak_mimic("Please say who you are")
+            Voice.speak_flite("Please say who you are")
     elif "what is my age" in response:
         if Age:
-            Voice.speak_mimic("Your Age is " + str(Age))
+            Voice.speak_flite("Your Age is " + str(Age))
         else:
-            Voice.speak_mimic("Please say who you are")
+            Voice.speak_flite("Please say who you are")
     elif "what is my name" in response:
         if Name:
             if Name == "Ayaan":
-                Voice.speak_mimic("Your Name is Aion")
+                Voice.speak_flite("Your Name is Aion")
             elif Name == "Zara":
-                Voice.speak_mimic("You Name is Zarah")
+                Voice.speak_flite("You Name is Zarah")
             else:
-                Voice.speak_mimic("Your Name is " + Name)
+                Voice.speak_flite("Your Name is " + Name)
         else:
-            Voice.speak_mimic("Please say who you are")
+            Voice.speak_flite("Please say who you are")
 
