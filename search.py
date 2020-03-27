89dbe0ce528d4089943bfd4fa0481b8d2908df4f
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-03-27 22:37:29 +0500

    Update search.py

--- search.py
+++ search.py
@@ -1,6 +1,8 @@
 import wikipedia
 from speak import Voice
 import random
+import urllib.request
+import re
 def search_wikipedia(search):
     if "who is" in search or "who was" in search or "who are" in search or "what is" in search or "what was" in search or "history of" in search:
         search = search.split()[2:]
