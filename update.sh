0c57e01d472f1b9f2f1a9cdf16930765115f90b5
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-04-13 17:13:56 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 sudo rm -rf /DOM
 cd / &&  git clone https://github.com/AlternateRacoon/DOM.git
+cd / && rm users.db
 cd / && wget https://github.com/AlternateRacoon/DOM/raw/master/users.db
 cd /DOM && python3 core.py
