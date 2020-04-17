3f9e940b7a9cc83601b9376344bf1612dc95ac38
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-04-18 02:23:42 +0500

    Update search.py

--- search.py
+++ search.py
@@ -145,42 +145,148 @@ def search_google(search):
             return
 
 def search_wikipedia(search):
+def search_google(search):
+    search = search.replace(" ","+")
+    if "how" in search:
+        if " to " in search:
+            html_data = urllib.request.urlopen(
+                urllib.request.Request("https://www.google.com/search?q=" + search,
+                                       headers={'User-Agent': 'Mozilla/5.0'})).read().decode(
+                "utf8")
+            ans = re.findall(
+                '<div class="BNeawe s3v9rd AP7Wnd"><span class="FCUp0c rQMQod">(.*?)</div></li></ol>',
+                html_data)
+            if ans:
+                ans = ans[0]
+                ans = ans.replace('<span class="FCUp0c rQMQod">', '')
+                ans = ans.replace('<span>', '')
+                ans = ans.replace('</span>', '')
+                ans = ans.replace('</div></div><div class="Ap5OSd"><ol class="mSx1Ee v7pIac"><li class="v9i61e"><div class="BNeawe s3v9rd AP7Wnd">', '')
+                ans = ans.replace('</div></li><li class="v9i61e"><div class="BNeawe s3v9rd AP7Wnd">', '')
+                ans = ans.replace('</div></li><li><div class="BNeawe s3v9rd AP7Wnd">', '')
+                ans = ans.replace('</div>','')
+                ans = ans.replace('<div><ol class="mSx1Ee v7pIac"><li class="v9i61e"><div class="BNeawe s3v9rd AP7Wnd">','')
+                return ans
+            else:
+                return
+        else:
+            link = "https://www.google.com/search?q=" + search
+            html_data = urllib.request.urlopen(
+                urllib.request.Request(link,
+                                       headers={'User-Agent': 'Mozilla/5.0'})).read().decode(
+                "utf8")
+            ans = re.findall('<div class="BNeawe s3v9rd AP7Wnd"><div><div class="BNeawe s3v9rd AP7Wnd">(.*?)</div></div></div></div></div><div', html_data)
+            if ans:
+                ans = ans[0]
+                ans = ans.replace('<span class="FCUp0c rQMQod">', '')
+                ans = ans.replace('<span>','')
+                ans = ans.replace('</span>', '')
+                return ans
+            else:
+                ans = re.findall(
+                    '</span>(.*?)</div></div></div></div></div></div></div></div><div><div class="',
+                    html_data)
+                if ans:
+                    ans = ans[-3]
+                    ans = ans.replace('<span class="FCUp0c rQMQod">', '')
+                    ans = ans.replace('<span>', '')
+                    ans = ans.replace('</span>', '')
+                    ans = ans.replace('<span class="r0bn4c rQMQod"> ', '')
+                    return ans
+                else:
+                    return
+    if "when" in search:
+        html_data = urllib.request.urlopen(
+            urllib.request.Request("https://www.google.com/search?q=" + search,
+                                   headers={'User-Agent': 'Mozilla/5.0'})).read().decode(
+            "utf8")
+        ans = re.findall('AP7Wnd">(.*?)</div>', html_data)
+        if ans:
+            if '</span><span class="BNeawe s3v9rd AP7Wnd">' in ans[0]:
+                ans = ans[1]
+            else:
+                ans = ans[0]
+            ans = ans.replace('<div><div class="BNeawe iBp4i AP7Wnd">', '')
+            ans = ans.replace('<div class="Ap5OSd"><div class="BNeawe s3v9rd AP7Wnd">', '')
+            ans = ans.replace('<span class="FCUp0c rQMQod">', '')
+            ans = ans.replace('<span>','')
+            ans = ans.replace('</span>', '')
+            ans = ans.replace('<div class="Ap5OSd">', '')
+            return ans
+        else:
+            return
+    elif "is" in search:
+        html_data = urllib.request.urlopen(
+            urllib.request.Request("https://www.google.com/search?q=" + search,
+                                   headers={'User-Agent': 'Mozilla/5.0'})).read().decode(
+            "utf8")
 
-    if "who is" in search or "who was" in search or "who are" in search or "what is" in search or "what was" in search or "history of" in search or "how many" in search or "how much" in search or "what do they" in search or "how to" in search or "how was" in search or "why did" in search or "how is" in search or "how can" in search or "when is" or "how" in search or "why" in search or "when" in search or "is" in search:
-        google_search = search_google(search)
-        if google_search:
-            Voice.speak_flite(google_search)
+        ans = re.findall('<div class="BNeawe s3v9rd AP7Wnd">(.*?)</div></div></div></div></div>', html_data)
+        if ans:
+            ans = ans[0]
+            ans = ans.replace('<span class="FCUp0c rQMQod">', '')
+            ans = ans.replace('<span>','')
+            ans = ans.replace('</span>', '')
+            ans = ans.replace('<div class="BNeawe s3v9rd AP7Wnd">','')
+            ans = ans.replace('<div>', '')
+            ans = ans.replace('</div>', '')
+            return ans
         else:
-            search = search.split()[2:]
-            search = " ".join(search)
-            info = ""
-            try:
-                info = wikipedia.summary(search, sentences=2)
-            except wikipedia.DisambiguationError as e:
-                s = random.choice(e.options)
-                info = wikipedia.summary(s, sentences=2)
-            except wikipedia.PageError as e:
+            return
+    elif "why" in search:
+        html_data = urllib.request.urlopen(
+            urllib.request.Request("https://www.google.com/search?q=" + search,
+                                   headers={'User-Agent': 'Mozilla/5.0'})).read().decode(
+            "utf8")
+        ans = re.findall('<div class="BNeawe s3v9rd AP7Wnd">(.*?)</div></div></div></div></div>', html_data)
+        if ans:
+            ans = ans[0]
+            ans = ans.replace('<span class="FCUp0c rQMQod">', '')
+            ans = ans.replace('<span>','')
+            ans = ans.replace('</span>', '')
+            ans = ans.replace('<div class="BNeawe s3v9rd AP7Wnd">','')
+            ans = ans.replace('<div>', '')
+            ans = ans.replace('</div>', '')
+            ans = ans.replace('<span class="r0bn4c rQMQod">','')
+            return ans
+        else:
+            return
+def search_wikipedia(search):
+    if "who is" in search or "who was" in search or "who are" in search or "what is" in search or "what was" in search or "history of" in search:
+        search = search.split()[2:]
+        search = " ".join(search)
+        Voice.speak_flite("Searching about " + search)
+        info = ""
+        try:
+            info = wikipedia.summary(search, sentences=2)
+        except wikipedia.DisambiguationError as e:
+            s = random.choice(e.options)
+            info = wikipedia.summary(s, sentences=2)
+        except wikipedia.PageError as e:
+            google_search = search_google(search)
+            if google_search:
+                Voice.speak_flite(google_search)
+            else:
                 Voice.speak_flite("Could not find any results")
-            if info:
-                Voice.speak_flite(info)
+        if info:
+            Voice.speak_flite(info)
     elif "tell me about" in search:
-        google_search = search_google(search)
-        if google_search:
-            Voice.speak_flite(google_search)
-        else:
-            search = search.split()[3:]
-            search = " ".join(search)
-            info = ""
-            try:
-                info = wikipedia.summary(search, sentences=2)
-            except wikipedia.DisambiguationError as e:
-                s = random.choice(e.options)
-                info = wikipedia.summary(s, sentences=2)
-            except wikipedia.PageError as e:
+        search = " ".join(search.split()[2:])
+        Voice.speak_flite("Searching about " + search)
+        info = ""
+        try:
+            info = wikipedia.summary(search, sentences=2)
+        except wikipedia.DisambiguationError as e:
+            s = random.choice(e.options)
+            info = wikipedia.summary(s, sentences=2)
+        except wikipedia.PageError as e:
+            google_search = search_google(search)
+            if google_search:
+                Voice.speak_flite(google_search)
+            else:
                 Voice.speak_flite("Could not find any results")
-            if info:
-                Voice.speak_flite(info)
-    return "who is" in search or "who was" in search or "who are" in search or "what is" in search or "what was" in search or "history of" in search or "tell me about" in search
+        if info:
+            Voice.speak_flite(info)
 def get_recipe(recipe_name):
     recipe_name = recipe_name.replace(" ", "%20")
     html_data = urllib.request.urlopen(
