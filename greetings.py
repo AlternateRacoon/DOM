a95857c0cabcdb6f3faa6befe760d58c4f6b64a7
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-04-20 22:09:48 +0500

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -35,74 +35,58 @@ def greet_user(response):
         for row in greetings[0]:
             if response in row:
                 check = True
-                text = Voice.speak_flite("Hello I am Dom", "Nice To meet You")
-                return check, text
+                return "Hello I am Dom, Nice To meet You"
         for row in greetings[1]:
             if response in row:
                 check = True
-                text = Voice.speak_flite("I am Fine", "How About You?")
-                return check, text
+                return "I am Fine, How About You?"
         for row in greetings[2]:
             if response in row:
                 check = True
-                text = Voice.speak_flite("Nothing much")
-                return check, text
+                return "Nothing much"
         for row in greetings[3]:
             if response in row:
                 check = True
-                text = Voice.speak_flite("Everything is fine")
-                return check, text
+                return "Everything is fine"
         for row in greetings[4]:
             if response in row:
                 check = True
-                text = Voice.speak_flite("My Day is Just Fine")
-                return check, text
-                    
+                return "My Day is Just Fine"
         for row in greetings[-2]:
             if response in row:
                 check = True
-                text = Voice.speak_flite("Nice to Meet you too")
-                return check, text
+                return "Nice to Meet you too"
         if "how have you been" in response:
             check = True
-            text = Voice.speak_flite("I have been fine", "how about you")
-            return check, text
+            return "I have been fine, how about you"
         if "morning" in response or "afternoon" in response or "evening" in response:
             check = True
             hour = datetime.datetime.now().hour
             greeting = "Its morning", "Good morning" if 5 <= hour < 12 else "Its Afternoon", "Good afternoon" if hour < 18 else "Its Evening", "Good evening"
-            text = Voice.speak_flite(greeting)
-            return check, text
+            return greeting
         if "hello" in response:
             check = True
-            text = Voice.speak_flite("Hello I am Dom", "Nice To meet You")
-            return check, text
+            return "Hello I am Dom, Nice To meet You"
         if "how are you" in response:
             check = True
-            text = Voice.speak_flite("I have been fine", "what about you")
-            return check, text
+            return "I have been fine, what about you"
         if "fine" in response:
             check = True
             if "i am" in response:
-                text = Voice.speak_flite("Good To Know")
-                return check, text
+                return "Good To Know"
             if "are you" in response:
-                text = Voice.speak_flite("Yes I am Fine")
-                return check, text
+                return "Yes I am Fine"
         if "ok" in response:
             check = True
             if "i am" in response:
-                text = Voice.speak_flite("Good To Know")
-                return check, text
+                return "Good To Know"
             if "are you" in response:
-                text = Voice.speak_flite("Yes I am Fine")
-                return check, text
+                return "Yes I am Fine"
         if "who are you" in response:
             check = True
-            text = Voice.speak_flite("I am Dom", "Dom stands for Data Operating Module")
-            return check, text
+            return "I am Dom, Dom stands for Data Operating Module"
         if "who made you" in response:
-            text = Voice.speak_flite("I am Just A Home Project")
-            return check, text
+            check = True
+            return "I am Just A Home Project"
         if not check:
             return False
