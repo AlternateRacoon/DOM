f8e81197fcf52666875221fc9a24014d093950d5
AlternateRacoon <52130858+AlternateRacoon@users.noreply.github.com>
2020-04-20 21:09:44 +0500

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -35,74 +35,74 @@ def greet_user(response):
         for row in greetings[0]:
             if response in row:
                 check = True
-                Voice.speak_flite("Hello I am Dom", "Nice To meet You")
-                return check
+                text = Voice.speak_flite("Hello I am Dom", "Nice To meet You")
+                return check, text
         for row in greetings[1]:
             if response in row:
                 check = True
-                Voice.speak_flite("I am Fine", "How About You?")
-                return check
+                text = Voice.speak_flite("I am Fine", "How About You?")
+                return check, text
         for row in greetings[2]:
             if response in row:
                 check = True
-                Voice.speak_flite("Nothing much")
-                return check
+                text = Voice.speak_flite("Nothing much")
+                return check, text
         for row in greetings[3]:
             if response in row:
                 check = True
-                Voice.speak_flite("Everything is fine")
-                return check
+                text = Voice.speak_flite("Everything is fine")
+                return check, text
         for row in greetings[4]:
             if response in row:
                 check = True
-                Voice.speak_flite("My Day is Just Fine")
-                return check
+                text = Voice.speak_flite("My Day is Just Fine")
+                return check, text
                     
         for row in greetings[-2]:
             if response in row:
                 check = True
-                Voice.speak_flite("Nice to Meet you too")
-                return check
+                text = Voice.speak_flite("Nice to Meet you too")
+                return check, text
         if "how have you been" in response:
             check = True
-            Voice.speak_flite("I have been fine", "how about you")
-            return check
+            text = Voice.speak_flite("I have been fine", "how about you")
+            return check, text
         if "morning" in response or "afternoon" in response or "evening" in response:
             check = True
             hour = datetime.datetime.now().hour
             greeting = "Its morning", "Good morning" if 5 <= hour < 12 else "Its Afternoon", "Good afternoon" if hour < 18 else "Its Evening", "Good evening"
-            Voice.speak_flite(greeting)
-            return check
+            text = Voice.speak_flite(greeting)
+            return check, text
         if "hello" in response:
             check = True
-            Voice.speak_flite("Hello I am Dom", "Nice To meet You")
-            return check
+            text = Voice.speak_flite("Hello I am Dom", "Nice To meet You")
+            return check, text
         if "how are you" in response:
             check = True
-            Voice.speak_flite("I have been fine", "what about you")
-            return check
+            text = Voice.speak_flite("I have been fine", "what about you")
+            return check, text
         if "fine" in response:
             check = True
             if "i am" in response:
-                Voice.speak_flite("Good To Know")
-                return check
+                text = Voice.speak_flite("Good To Know")
+                return check, text
             if "are you" in response:
-                Voice.speak_flite("Yes I am Fine")
-                return check
+                text = Voice.speak_flite("Yes I am Fine")
+                return check, text
         if "ok" in response:
             check = True
             if "i am" in response:
-                Voice.speak_flite("Good To Know")
-                return check
+                text = Voice.speak_flite("Good To Know")
+                return check, text
             if "are you" in response:
-                Voice.speak_flite("Yes I am Fine")
-                return check
+                text = Voice.speak_flite("Yes I am Fine")
+                return check, text
         if "who are you" in response:
             check = True
-            Voice.speak_flite("I am Dom", "Dom stands for Data Operating Module")
-            return check
+            text = Voice.speak_flite("I am Dom", "Dom stands for Data Operating Module")
+            return check, text
         if "who made you" in response:
-            Voice.speak_flite("I am Just A Home Project")
-            return check
+            text = Voice.speak_flite("I am Just A Home Project")
+            return check, text
         if not check:
             return False
